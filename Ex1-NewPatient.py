c342a424919f3d69501aebf96293d6c5e203171e
chandraadari <cadari@gmail.com>
2020-08-16 14:45:54 +0100

    new paitent excersise

---
+++ Ex1-NewPatient.py
@@ -0,0 +1,13 @@
+'''
+Requirement:
+    We need to check in a patient named John Smith.
+    He is 20 years old and is a new patient.
+'''
+
+name = "John Smith"
+age = 20
+isNewPatient = True
+
+print ( "Name of the patient: " + name )
+print ( "Age: " + str(age))
+print ( "Is a new patient: " + str(isNewPatient))
--- app.py
+++ app.py
@@ -8,14 +8,15 @@ print ("'#'______/")
 print("  /|  /| ")
 
 print(  10 * "^")
+name = "mouli"
 
 price = 10
 rating = 4.9
-
-name = "mouli"
-
-
 is_public = False
+print (price * rating * is_public)   # The value will be zero
+
+is_public = True
+print (price * rating * is_public)   # The value will be 49.0
+
 
 print (is_public)
-print (price * rating)
