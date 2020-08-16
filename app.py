7b66d1ce11a1c4648360190f9bf42dc3eff355ad
chandraadari <cadari@gmail.com>
2020-08-16 13:08:09 +0100

    general print examples

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
+'''
 print("Chandra Mouli Adari")
 print('Chandra Mouli Adari')
-print ("E'______/")
-print("  /\  /\ ")
+
 print("*" * 10)
+'''
+print ("'#'______/")
+print("  /|  /| ")
+
+print(  10 * "^")
+
+price = 10
+rating = 4.9
+
+name = "mouli"
+
+
+is_public = False
+
+print (is_public)
+print (price * rating)
